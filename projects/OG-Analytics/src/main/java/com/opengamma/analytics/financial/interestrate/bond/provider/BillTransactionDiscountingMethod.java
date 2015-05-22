9dbb6266ff8d014b971afe107b0991f8b7f15d9c
Elaine McLeod <em.mcleod@gmail.com>
2015-05-22 16:46:31 +0100

    Fixing unit tests

--- projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/interestrate/bond/provider/BillTransactionDiscountingMethod.java
+++ projects/OG-Analytics/src/main/java/com/opengamma/analytics/financial/interestrate/bond/provider/BillTransactionDiscountingMethod.java
@@ -2,6 +2,10 @@
  * Copyright (C) 2012 - present by OpenGamma Inc. and the OpenGamma group of companies
  *
  * Please see distribution for license.
+ *
+ * Modified by McLeod Moores Software Limited.
+ *
+ * Copyright (C) 2015-Present McLeod Moores Software Limited.  All rights reserved.
  */
 package com.opengamma.analytics.financial.interestrate.bond.provider;
 
