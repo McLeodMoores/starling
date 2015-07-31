f07bef334d66719b262deba4c07062d124b2e369
Jim Moores <jim.moores@gmail.com>
2015-07-31 13:42:54 +0100

    Changed back temporarily.

--- projects/OG-EngineCommon/src/main/java/com/opengamma/engine/marketdata/AlwaysAvailableMarketDataProvider.java
+++ projects/OG-EngineCommon/src/main/java/com/opengamma/engine/marketdata/AlwaysAvailableMarketDataProvider.java
@@ -69,8 +69,8 @@ public class AlwaysAvailableMarketDataProvider extends AbstractMarketDataProvide
 
   @Override
   public MarketDataSnapshot snapshot(MarketDataSpecification marketDataSpec) {
-    return new MockMarketDataSnapshot(new MockMarketDataProvider("AlwaysAvailableMarketDataProvider", true, 0));
-//    throw new UnsupportedOperationException("This data provider only exists to produce a valid dependency graph build, not to provide data.");
+    //return new MockMarketDataSnapshot(new MockMarketDataProvider("AlwaysAvailableMarketDataProvider", true, 0));
+    throw new UnsupportedOperationException("This data provider only exists to produce a valid dependency graph build, not to provide data.");
   }
 
 }
