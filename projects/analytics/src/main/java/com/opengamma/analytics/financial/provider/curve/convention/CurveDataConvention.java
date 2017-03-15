527eab72a4f50f698185238512ec1656c15797e1
Elaine McLeod <em.mcleod@gmail.com>
2017-03-15 12:54:51 +0000

    Commit to pull

---
+++ projects/analytics/src/main/java/com/opengamma/analytics/financial/provider/curve/convention/CurveDataConvention.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.opengamma.analytics.financial.provider.curve.convention;
+
+/**
+ * @author emcleod
+ *
+ */
+public interface CurveDataConvention {
+
+}
---
+++ projects/analytics/src/main/java/com/opengamma/analytics/financial/provider/curve/data/InterestRateCurveDataType.java
@@ -0,0 +1,26 @@
+/**
+ *
+ */
+package com.opengamma.analytics.financial.provider.curve.data;
+
+import org.threeten.bp.Period;
+
+import com.opengamma.analytics.financial.instrument.index.GeneratorInstrument;
+import com.opengamma.util.money.Currency;
+
+/**
+ * @author emcleod
+ *
+ */
+public enum InterestRateCurveDataType /*implements CurveDataType*/ {
+
+  OVERNIGHT_DEPOSIT {
+    //@Override
+    public GeneratorInstrument<?> getInstrumentGenerator(final String name, final Currency currency, final Period period) {
+      return null; //new GeneratorDepositON(name, currency, holidays);
+    }
+  },
+
+  OIS;
+
+}
